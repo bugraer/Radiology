c6036eb98adc929a21c95f2a0574e40365a4eb4d
Buğra ER <67065146+bugraer@users.noreply.github.com>
2021-05-25 22:50:40 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace derle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(639, 221);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(482, 358);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(305, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(391, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(469, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(559, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(643, 270);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(733, 270);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(121, 310);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(216, 310);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "label10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateBlue;
+            this.ClientSize = new System.Drawing.Size(853, 391);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelApp = Microsoft.Office.Interop.Excel;
+namespace derle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        double l=0;
+        double sutun1dogru, sutun1say;
+        string[] sutun1 = new string[10];
+        double[] sutun1d = new double[10];
+        double o;
+
+
+      //  double l1 = 0;
+        double sutun2dogru, sutun2say;
+       /* string[] sutun2 = new string[10];
+        double[] sutun2d = new double[10];
+        */
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            string DosyaYolu;
+            string DosyaAdi;
+            DataTable dt;
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel Dosyası | *.xls; *.xlsx; *.xlsm";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                DosyaYolu = file.FileName;// seçilen dosyanın tüm yolunu verir
+                DosyaAdi = file.SafeFileName;// seçilen dosyanın adını verir.
+                ExcelApp.Application excelApp = new ExcelApp.Application();
+                if (excelApp == null)
+                { //Excel Yüklümü Kontrolü Yapılmaktadır.
+                    MessageBox.Show("Excel yüklü değil.");
+                    return;
+                }
+
+                //Excel Dosyası Açılıyor.
+                ExcelApp.Workbook excelBook = excelApp.Workbooks.Open(DosyaYolu);
+                //Excel Dosyasının Sayfası Seçilir.
+                ExcelApp._Worksheet excelSheet = excelBook.Sheets[1];
+                //Excel Dosyasının ne kadar satır ve sütun kaplıyorsa tüm alanları alır.
+                ExcelApp.Range excelRange = excelSheet.UsedRange;
+
+                int satirSayisi = excelRange.Rows.Count; //Sayfanın satır sayısını alır.
+                int sutunSayisi = excelRange.Columns.Count; //Sayfanın sütun sayısını alır.
+                dt = ToDataTable(excelRange, satirSayisi, sutunSayisi);
+
+                dataGridView1.DataSource = dt;
+                dataGridView1.Refresh();
+
+                //Okuduktan Sonra Excel Uygulamasını Kapatıyoruz.
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+
+            }
+            else
+            {
+                MessageBox.Show("Dosya Seçilemedi.");
+            }
+        }
+
+        public DataTable ToDataTable(ExcelApp.Range range, int rows, int cols)
+        {
+            DataTable table = new DataTable();
+
+            for (int i = 1; i <= rows; i++)
+            {
+                if (i == 1)
+                { // ilk satırı Sutun Adları olarak kullanıldığından bunları Sutün Adları Olarak Kaydediyoruz.
+                    for (int j = 1; j <= cols; j++)
+                    {
+                        //Sütunların içeriği boş mu kontrolü yapılmaktadır.
+                        if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                            table.Columns.Add(range.Cells[i, j].Value2.ToString());
+                        else
+                            table.Columns.Add(j.ToString() + ".Sütun"); //Boş olduğunda Kaçınsı Sutünsa Adı veriliyor.
+                    }
+                    continue;
+                }
+
+                //Yukarıda Sütunlar eklendi onun şemasına göre yeni bir satır oluşturuyoruz. 
+                //Okunan verileri yan yana sıralamak için
+                var yeniSatir = table.NewRow();
+                for (int j = 1; j <= cols; j++)
+                {
+                    //Sütunların içeriği boş mu kontrolü yapılmaktadır.
+                    if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                        yeniSatir[j - 1] = range.Cells[i, j].Value2.ToString();
+                    else
+                        yeniSatir[j - 1] = String.Empty; // İçeriği boş hücrede hata vermesini önlemek için
+                }
+                table.Rows.Add(yeniSatir);
+
+            }
+
+
+            for (int suttun = 6; suttun <= 15; suttun++)
+            {
+                l = 0;
+                o = 0;
+                sutun1say = 0;
+                    sutun1dogru = 0;
+                sutun2say = 0;
+                sutun2dogru = 0;
+                string[] k = new string[9];
+                for (int g = 2; g <= 5; g++)
+                {
+                    k[g] = range.Cells[g, suttun].Value2.ToString();
+                    l = l + Convert.ToDouble(k[g]);
+
+                }
+                o = l / 4;
+
+
+                for (int gg = 2; gg <= 5; gg++)
+                {
+                    if (range.Cells[gg, 4].Value2.ToString() == "luminal A")
+                    {
+
+                        sutun1[gg] = range.Cells[gg, suttun].Value2.ToString();
+                        sutun1dogru++;
+                        if (Convert.ToDouble(sutun1[gg]) < o)
+                        {
+                            sutun1say++;
+                        }
+
+                    }
+                }
+                for (int ggg = 2; ggg <= 5; ggg++)
+                {
+                    if (range.Cells[ggg, 4].Value2.ToString() == "luminal B")
+                    {
+
+                        sutun1[ggg] = range.Cells[ggg, suttun].Value2.ToString();
+                        sutun2dogru++;
+                        if (Convert.ToDouble(sutun1[ggg]) > o)
+                        {
+                            sutun2say++;
+                        }
+
+                    }
+                }
+                if ((sutun1dogru == sutun1say) && (sutun2dogru == sutun2say))
+                {
+                    if (suttun == 6)
+                    {
+                        label1.Text = range.Cells[1, 6].Value2.ToString();
+                    }
+                    if (suttun == 7)
+                    {
+                        label2.Text = range.Cells[1, 7].Value2.ToString();
+                    }
+                    if (suttun == 8)
+                    {
+                        label3.Text = range.Cells[1, 8].Value2.ToString();
+                    }
+                    if (suttun ==9)
+                    {
+                        label4.Text = range.Cells[1, 9].Value2.ToString();
+                    }
+                    if (suttun == 10)
+                    {
+                        label5.Text = range.Cells[1, 10].Value2.ToString();
+                    }
+                    if (suttun == 11)
+                    {
+                        label6.Text = range.Cells[1, 11].Value2.ToString();
+                    }
+                    if (suttun == 12)
+                    {
+                        label7.Text = range.Cells[1, 12].Value2.ToString();
+                    }
+                    if (suttun == 13)
+                    {
+                        label8.Text = range.Cells[1, 13].Value2.ToString();
+                    }
+                    if (suttun == 14)
+                    {
+                        label9.Text = range.Cells[1, 14].Value2.ToString();
+                    }
+                    if (suttun == 15)
+                    {
+                        label10.Text = range.Cells[1, 15].Value2.ToString();
+                    }
+                }
+                else
+                {
+                    if (suttun == 6)
+                    {
+                        label1.Text = "";
+                    }
+                    if (suttun == 7)
+                    {
+                        label2.Text = "";
+                    }
+                    if (suttun == 8)
+                    {
+                        label3.Text = "";
+                    }
+                    if (suttun == 9)
+                    {
+                        label4.Text = "";
+                    }
+                    if (suttun == 10)
+                    {
+                        label5.Text = "";
+                    }
+                    if (suttun == 11)
+                    {
+                        label6.Text = "";
+                    }
+                    if (suttun == 12)
+                    {
+                        label7.Text = "";
+                    }
+                    if (suttun == 13)
+                    {
+                        label8.Text = "";
+                    }
+                    if (suttun == 14)
+                    {
+                        label9.Text = "";
+                    }
+                    if (suttun == 15)
+                    {
+                        label10.Text = "";
+                    }
+                }
+               // label14.Text = String.Format(Math.Round(l, 0).ToString());
+            }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   /*         string[] k1 = new string[9];
+            for (int g = 2; g <= 5; g++)
+            {
+                k1[g] = range.Cells[g, 8].Value2.ToString();
+                l1 = l1 + Convert.ToDouble(k1[g]);
+
+            }
+            double o1 = l1 / 4;
+            label3.Text = String.Format(Math.Round(o1, 0).ToString());
+
+            for (int g = 2; g <= 5; g++)
+            {
+                if (range.Cells[g, 4].Value2.ToString() == "luminal A")
+                {
+
+                    sutun2[g] = range.Cells[g, 8].Value2.ToString();
+                    sutun2dogru++;
+                    if (Convert.ToDouble(sutun2[g]) < l)
+                    {
+                        sutun2say++;
+                    }
+
+                }
+            }
+            if (sutun2dogru == sutun2say)
+            {
+                label13.Text = "bumbe yarag";
+            }
+
+
+
+*/
+
+
+            /*
+
+
+
+                            string[] x = new string[4];
+                        string[] y = new string[4];
+                        string[] z = new string[4];
+                        int[] AA = new int[4];
+                        int[] BB = new int[4];
+                        {
+                            x[0] = range.Cells[2, 6].Value2.ToString();
+                            x[1] = range.Cells[3, 6].Value2.ToString();
+                            x[2] = range.Cells[4, 6].Value2.ToString();
+                            x[3] = range.Cells[5, 6].Value2.ToString();
+
+                            string abc = range.Cells[3, 4].Value2.ToString();
+                            if(abc == "luminal B")
+                            {
+                                label12.Text = "5";
+                            }
+                            label13.Text = range.Cells[1, 4].Value2.ToString();
+                        }
+                        double a = Convert.ToDouble(x[0]) + Convert.ToDouble(x[1]) + Convert.ToDouble(x[2]) + Convert.ToDouble(x[3]);
+                        a = a / 4;
+            label1.Text = String.Format(Math.Round(a, 0).ToString());
+
+                        for (int kkk = 2; kkk <= 5; kkk++)
+                        {
+
+                        }
+
+
+                        label5.Text = String.Format(k[2].ToString());
+                        label7.Text = String.Format(k[3].ToString());
+                        label9.Text = String.Format(k[4].ToString());
+
+                        */
+            return table;
+        }
+
+      
+
+    
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
